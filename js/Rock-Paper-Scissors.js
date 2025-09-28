d78c14a46d5bc2f50d4148363b625fe8f32f48e7
omoladeajala <omoladeajala2021@gmail.com>
2025-09-29 00:15:33 +0100

    Implement getRandomInt function and log random choices for Rock-Paper-Scissors game

--- js/Rock-Paper-Scissors.js
+++ js/Rock-Paper-Scissors.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+console.log(getRandomInt("Rock", "Paper", "Scissors")); 
+
+console.log(getRandomInt("Rock")); 
+
+console.log(getRandomInt("Paper")); 
+
+console.log(getRandomInt("Scissors")); 
