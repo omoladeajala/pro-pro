bc26e02151ed175fe8491c768b591a7b2f3ad1aa
omoladeajala <omoladeajala2021@gmail.com>
2025-09-29 01:03:17 +0100

    Refactor getComputerChoice function and update getHumanChoice for user input

--- js/Rock-Paper-Scissors.js
+++ js/Rock-Paper-Scissors.js
@@ -1,11 +1,33 @@
-function getHumanChoice(max) {
-  return Math.floor(Math.random() * max)
+function getComputerChoice() {
+  const randomNumber = Math.floor();
+  let computerChoice;
+
+
+if (randomNumber <1/3) {
+    computerChoice = "rock";
+} else if (randomNumber <2/3) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors"
 }
 
-console.log(getHumanChoice("Rock", "Paper", "Scissors")); 
+return computerChoice;
+}
 
-console.log(getHumanChoice("Rock")); 
+function getHumanChoice() {
+    const userChoice = prompt("Enter your chice: Rock, Paper, or Scissors");
+    return userChoice;
+}
 
-console.log(getHumanChoice("Paper")); 
+console.log(getHumanChoice());
 
-console.log(getHumanChoice("Scissors")); 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+ //my codes here!
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
