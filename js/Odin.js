83fd583dc180bf65ce2901d5d50008fba1ca54e8
omoladeajala <omoladeajala2021@gmail.com>
2025-10-06 08:44:29 +0100

    Add initial HTML, CSS, and JavaScript files for To-Do List application

--- js/Odin.js
+++ js/Odin.js
@@ -1,4 +1,20 @@
-const name = "Chris";
-function greeting() {
-    alert(`Hello ${name}: welcome to our company.`)
+const output = document.querySelector(".output");
+output.textContent = "";
+
+let i = 10;
+
+while ( i >= 10 ) {
+        const para =
+        document.createElement('p');
+        if ( i === 10) {
+            para.textContent = `Countdown ${i}`;
+    } else if ( i === 0 ) {
+        para.textContent = "Blast off!";
+    } else { 
+        para.textContent = i;
+    } 
+
+    output.apprendChild(para);
+
+    i--;
 }
---
+++ js/To-Do List.js
@@ -0,0 +1,11 @@
+const inputBox = document.getElementById('inputBox');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+
+
+
+const addTodo = ()=>{
+    alert("Hello");
+}
+
+addBtn.addEventListener('click', addTodo);
