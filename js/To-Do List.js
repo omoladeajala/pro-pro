61d36c9e0919038273043db1b8cc3935d2e9e254
omoladeajala <omoladeajala2021@gmail.com>
2025-10-07 13:32:41 +0100

    Refactor To-Do List functionality to improve task addition and validation; update CSS for better layout

--- js/To-Do List.js
+++ js/To-Do List.js
@@ -5,7 +5,33 @@ const todoList = document.getElementById('todoList');
 
 
 const addTodo = ()=>{
-    alert("Hello");
+    const inputText = inputBox.value.trim();
+    if(inputText.length <= 0){
+        alert("You must write something in your to do");
+        return false;
+    }
+
+    //Creating p tag 
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerHTML = inputText;
+    li.appendChild(p);
+
+    //Creating delete Btn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Remove";
+    deleteBtn.classList.add("btn");
+    li.appendChild(deleteBtn);
+
+     //Creating edit Btn
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    li.appendChild(editBtn);
+
+    todoList.appendChild(li);
+    inputBox.value = ""; 
+
 }
+
 
 addBtn.addEventListener('click', addTodo);
