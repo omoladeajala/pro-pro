69511979362d3c36a374f450235d054fc66df64c
omoladeajala <omoladeajala2021@gmail.com>
2025-10-08 15:54:09 +0100

    Add dynamic DOM elements with styled pink div and text

--- js/Odin.js
+++ js/Odin.js
@@ -1,20 +1,29 @@
-const output = document.querySelector(".output");
-output.textContent = "";
+const container = document.getElementById('container');
+/
+//create a red paragraph
+const p = document.createElement('p');
+p.textContent= "Hey I'm red!";
+container.appendChild(p);
 
-let i = 10;
+//create a blue paragraph
+const h3 = document.createElement('h3');
+h3.textContent = "I'm a blue h3";
+container.appendChild(h3);
 
-while ( i >= 10 ) {
-        const para =
-        document.createElement('p');
-        if ( i === 10) {
-            para.textContent = `Countdown ${i}`;
-    } else if ( i === 0 ) {
-        para.textContent = "Blast off!";
-    } else { 
-        para.textContent = i;
-    } 
+const pinkDiv = document.createElement('div');
+pinkDiv.style.border = '2px solid black';
+pinkDiv.style.backgroundColor = 'pink';
+pinkDiv.style.padding = '10px';
+pinkDiv.style.marginTop = '10px';
 
-    output.apprendChild(para);
+const h1Inside = document.createElement('h1');
+h1Inside.textContent = "I'm in a div";
 
-    i--;
-}
+const pinside = document.createElement('p');
+pInside.textContent = "ME TOO!";
+
+
+pinkDik.appendChild(hiInside);
+pinkDiv.appendChild(pInside);
+
+container.appendChild(pinkDiv);
