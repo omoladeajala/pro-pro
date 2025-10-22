d909c144a6def81812c19350ca85960f831a1f07
omoladeajala <omoladeajala2021@gmail.com>
2025-10-22 12:21:22 +0100

    Implement basic arithmetic functions and operate  logic

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -16,3 +16,24 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+add(3, 5);
+divide(10, 0);
+
+function operate(operate, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operate) {
+      case '+':
+        return add(a,b);
+      case '+':
+        return subtract(a,b);
+      case '+':
+        return multiply(a,b);
+      case '+':
+        return divide(a,b);
+      default:
+        return null;
+    }
+}
